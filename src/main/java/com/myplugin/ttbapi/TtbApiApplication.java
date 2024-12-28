dda7084c4682c8a39b3e8aea4abf49fa6a4da069
송은혜 <dmsgp8292@gmail.com>
2024-12-28 12:24:59 +0900

    first

---
+++ src/main/java/com/myplugin/ttbapi/TtbApiApplication.java
@@ -0,0 +1,13 @@
+package com.myplugin.ttbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtbApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtbApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myplugin/ttbapi/TtbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myplugin.ttbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtbApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
