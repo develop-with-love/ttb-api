94b61bf0ad2f22de5b7263c5a2602799d41c36b1
msk102948 <msk102948@gmail.com>
2025-05-03 15:17:52 +0900

    Repository ID type 변환
    - String -> Long

--- src/main/java/com/ttb/api/aggregate/employee/adapter/output/repository/DutyRepository.java
+++ src/main/java/com/ttb/api/aggregate/employee/adapter/output/repository/DutyRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DutyRepository extends JpaRepository<DutyEntity, String> {
+public interface DutyRepository extends JpaRepository<DutyEntity, Long> {
 } 
--- src/main/java/com/ttb/api/aggregate/employee/adapter/output/repository/PositionRepository.java
+++ src/main/java/com/ttb/api/aggregate/employee/adapter/output/repository/PositionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PositionRepository extends JpaRepository<PositionEntity, String> {
+public interface PositionRepository extends JpaRepository<PositionEntity, Long> {
 } 
